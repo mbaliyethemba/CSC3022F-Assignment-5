87d7a5bc10b99e8ff62289bf106353483fff6e76
Themba <SHNMBA004@uct.ac.za>
2020-05-16 18:45:46 +0200

    Added the reader for january rainfall averages

--- pca.cpp
+++ pca.cpp
@@ -11,3 +11,19 @@ pca::pca(){}
 
 //destructor
 pca::~pca(){}
+
+//read the averages of rainfall in january
+void pca::read_january(){
+	std::ifstream janFile("January Observations.txt", std::ios::in);
+	if(janFile.is_open()){
+		std::string s;
+		while(std::getline(janFile, s)){
+			this->jan.push_back(std::stod(s));
+		}
+		janFile.close();
+		std::cout << "File has been read" << std::endl;
+	}
+	else{
+		std::cerr << "Unable to open file." << std::endl;
+	}
+}
