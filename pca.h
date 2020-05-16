306df27fec4573c94ef928fb34edd8441c7be3f9
Themba <SHNMBA004@uct.ac.za>
2020-05-16 18:32:17 +0200

    Added the constructor.

--- pca.h
+++ pca.h
@@ -1,1 +1,29 @@
+//Mbaliyethemba Shangase
+//SHNMBA004
+//Assignment 5
+//17 May 2020
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <functional>   // std::minus
+#include <numeric>      // std::accumulate
+#include "Eigen/Dense"
+
+#ifndef PCA_H
+#define PCA_H
+
+using namespace Eigen;
+
+class pca{
+	private:
+		std::vector<double> jan, jul;
+		std::vector<std::vector<double>> matrix;
+	
+	public:
+		pca(); //constructor
+};
+
+#endif
