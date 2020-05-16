e42d29409cf4757093c5bb698d004339d623aebc
Themba <SHNMBA004@uct.ac.za>
2020-05-16 19:05:14 +0200

    Added the function to find the mean

--- pca.cpp
+++ pca.cpp
@@ -34,12 +34,37 @@ void pca::read_july(){
 	if(julFile.is_open()){
 		std::string s;
 		while(std::getline(julFile, s)){
-			this->jul.push_back(std::stod(s));
+			this->july.push_back(std::stod(s));
 		}
 		julFile.close();
 		std::cout << "File has been read" << std::endl;
 	}
 	else{
 		std::cerr << "Unable to open file." << std::endl;
+	}
+}
+
+//finds the mean
+void pca::find_mean(){
+	double jansum;
+	//finds the sum for jan
+	for(size_t i = 0; i < this->jan.size(); i++){
+		jansum += this->jan[i];
+	}
+	double janmean = jansum / this->jan.size();
+	//restructor the jan vector using mean
+	for(size_t i = 0; i < this->jan.size(); i++){
+		this->jan[i] = janmean - this->jan[i];
+	}
+	
+	double julysum;
+	//finds the sum for jan
+	for(size_t i = 0; i < this->july.size(); i++){
+		julysum += this->july[i];
+	}
+	double julymean = julysum / this->july.size();
+	//restructor the jan vector using mean
+	for(size_t i = 0; i < this->july.size(); i++){
+		this->july[i] = julymean - this->july[i];
 	}
 }
