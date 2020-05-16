9ef09bb2876d094bf121a288bd5cc03818ea6233
Themba <SHNMBA004@uct.ac.za>
2020-05-16 19:15:03 +0200

    Added the driver functions

--- driver.cpp
+++ driver.cpp
@@ -1,1 +1,16 @@
+//Mbaliyethemba Shangase
+//SHNMBA004
+//Assignment 5
+//17 May 2020
 
+#include "pca.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char const *argv[]){
+	pca p; //constructs pca
+	p.read_january();
+	p.read_july();
+	p.find_mean();
+}
